b2a1ec7a63feaca96ade7d34258fc73809ff0d82
Peter Junior Mendy <petermendy5@gmail.com>
2023-02-25 13:52:39 +0000

    add a cors config

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 dotenv.config();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 mongoose.set("strictQuery", true);
